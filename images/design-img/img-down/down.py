cef937ade60852086f4494f470585da6a264a195
lzt1008 <lzt1008@live.com>
2021-06-07 13:23:35 +0800

    first commit

---
+++ images/design-img/img-down/down.py
@@ -0,0 +1,5 @@
+
+for i in range(24):
+    print(r"""<div class="image">
+                <img src="img-down/""" + str(i)+r""".jpg" alt="">
+            </div>""")
---
+++ js/coffee-blog.js
@@ -0,0 +1,49 @@
+var box = document.getElementById('header-box');
+var boxOffset = box.offsetTop;
+var cards = document.getElementsByClassName('card');
+
+window.onscroll = function (e) {
+    animations(e);
+}
+
+function animations(e) {
+    // 导航栏
+    var boxDistance = boxOffset - window.pageYOffset;
+    if (boxDistance <= 0) {
+        box.classList.add('fixed');
+    }
+    else {
+        box.classList.remove('fixed');
+    }
+
+    // 卡片动画
+    var index;
+    var newIndex = 0;
+    for (index = 0; index < cards.length; ++index) {
+        var offsetCard = offset(cards[index]);
+        if ((offsetCard.top <= window.pageYOffset + window.innerHeight) && !cards[index].classList.contains('active')) {
+            cards[index].setAttribute('style', 'transition-delay: ' + 0.1 * newIndex + 's');
+            cards[index].classList.add('active');
+            newIndex++;
+        }
+    }
+
+    // 进度条
+    var s = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    var body = document.body;
+    var html = document.documentElement;
+    var d = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+    var c = window.innerHeight;
+    var position = (s / (d - c)) * 100;
+    document.getElementById('progress').style.width = position + '%';
+    // document.getElementById('progress').style.display = 'block';
+}
+
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+animations();
